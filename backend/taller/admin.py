e068d8a91110647309162e15f03031b8c8464800
AleDevCV <acovi2014@yahoo.com>
2026-05-06 22:04:12 -0400

    Mostrar todas las tablas en el panel de admin

--- backend/taller/admin.py
+++ backend/taller/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# Obtenemos todos los modelos (tablas) de la aplicación actual ('taller')
+app_models = apps.get_app_config('taller').get_models()
+
+# Le decimos a Django que los registre todos en el panel
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
